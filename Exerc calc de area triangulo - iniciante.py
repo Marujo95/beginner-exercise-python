a7bcc8222952793b6772f5b98576174538e0b643
Marujo95 <166455008+Marujo95@users.noreply.github.com>
2024-05-10 17:45:08 -0300

    Add files via upload
    arquivos .py exercicios iniciantes de aprendizado

---
+++ Exerc calc de area triangulo - iniciante.py
@@ -0,0 +1,11 @@
+#exercicio para calculo de area de retangulo
+
+# 1. entrada de dados
+altura = float(input("Digite a altura: "))
+largura = float(input("Digite a largura: "))
+
+# 2. processamento
+area = altura * largura
+
+# 3. saída
+print("area =", area)
---
+++ Exerc numero par - iniciante.py
@@ -0,0 +1,10 @@
+#exercicio para saber se o numero é par
+
+# 1. entrada
+numero = int(input("Informe um numero inteiro: "))
+
+# 2. processamento
+par = (numero % 2 == 0)
+
+# 3. saída
+print("Este número é par?", par)
